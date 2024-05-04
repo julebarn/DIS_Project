63a6491482706fdff4743d0248608dcdd86d3bdf
mads jerup <madsjerup@gmail.com>
2024-05-04 15:06:18 +0200

    setup server and build tools

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" >
+
+
+    let msg = "no message"
+    function btn(e: Event) {
+        console.log(e)
+        
+        fetch("/api")
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data)
+                msg = data.message
+            })
+    }
+  
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<button on:click={btn}>Click me</button>
+
+{msg}
