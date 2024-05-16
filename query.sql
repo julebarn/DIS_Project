0953508a7e23062869bdcd5f79c98ba6225f1473
mads jerup <madsjerup@gmail.com>
2024-05-16 15:32:35 +0200

    login system

--- query.sql
+++ query.sql
@@ -1,15 +1,7 @@
--- name: GetAuthor :one
-SELECT * FROM authors
-WHERE id = $1 LIMIT 1;
 
--- name: ListAuthors :many
-SELECT * FROM authors
-ORDER BY name;
+-- name: GetUser :one
+SELECT * FROM users WHERE username = $1;
 
--- name: CreateAuthor :one
-INSERT INTO authors (
-  name, bio
-) VALUES (
-  $1, $2
-)
-RETURNING *;
+
+-- name: CreateUser :one
+INSERT INTO users (username, passwordHash) VALUES ($1, $2) RETURNING *;
